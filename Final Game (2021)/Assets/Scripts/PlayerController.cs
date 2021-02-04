dba413463de36f16593556b681039957cdb4e73f
Braxton Hunter <10922245@my.uvu.edu>
2021-02-03 23:46:44 -0700

    Added and Updated Player Controller

---
+++ Final Game (2021)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+    public float speed = 3f;
+    public float jumpForce = 300f;
+
+    private Vector2 direction;
+    private Vector2 yDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal") * speed;
+        ballRigidbody2D.AddForce(direction, ForceMode2D.Force);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            yDirection.y = jumpForce;
+            ballRigidbody2D.AddForce(yDirection, ForceMode2D.Force);
+        }
+    }
+}
